5f6d817862e78aa7d359e1a0ab312fd8edab5e71
PetarCehoo <petarceho@gmai.com>
2020-08-06 00:05:04 +0200

    implementation of a active object concurrent pattern.

--- src/ActiveObject.java
+++ src/ActiveObject.java
@@ -1,9 +1,16 @@
+/**
+ * Active object:
+ * decouples method execution from method invocation
+ * when you want to introduce async method invocation
+ *
+ *
+ * author:petar
+ */
+
+
+
+
 public class ActiveObject {
-
-
-
-
-
 
 
     public static void main(String []args)
