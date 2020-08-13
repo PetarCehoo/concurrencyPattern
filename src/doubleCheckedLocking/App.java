ea3f8630b25eb67555bcbf2faef02d4a3c18f07b
PetarCehoo <50662152+PetarCehoo@users.noreply.github.com>
2020-08-13 23:19:41 +0200

    Update App.java
    comments

--- src/doubleCheckedLocking/App.java
+++ src/doubleCheckedLocking/App.java
@@ -3,8 +3,9 @@ package doubleCheckedLocking;
 
 /**
  * Double checking locking with Signleton
- *
- *
+ * 
+ * DBC used to reduce the overhead of acquiring a lock by testing the locking criterion (the "lock hint") 
+ * before acquiring the lock. Locking occurs only if the locking criterion check indicates that locking is required.
  *
  *
  * author: petar
